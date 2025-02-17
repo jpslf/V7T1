2ee12a33bf721e5d3be25fdb695145db3ae8e165
jpslf <joona.pullinen@student.lut.fi>
2025-02-17 14:35:34 +0200

    Add neccessary functionality

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,12 +1,151 @@
 package main;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+
+abstract class Book {
+	String title;
+	String author;
+
+	int pages;
+	int copies;
+
+	void displayinfo() {
+		System.out.println(String.format("Nimi: %s, Kirjailija %s, Sivut %d, Määrä: %d", title, author, pages, copies));
+	}
+
+	abstract String categorize();
+}
+
+class FictionBook extends Book implements Borrowable {
+	int borrows;
+
+	FictionBook(String title_, String author_, int pages_, int copies_) {
+		title = title_;
+		author = author_;
+
+		pages = pages_;
+		copies = copies_;
+		borrows = 0;
+	}
+
+	@Override
+	String categorize() {
+		return "Fiktiokirja";
+	}
+
+	@Override
+	public void borrow() {
+		if(copies == 0) {
+			System.out.println(String.format("Kirjaa %s ei enään ole lainattavissa!", title));
+		} else {
+			copies -= 1;
+			borrows += 1;
+
+			System.out.println(String.format("Kirja %s on nyt lainattu.", title));
+		}
+	}
+
+	@Override
+	public void returnBook() {
+		if (borrows == 0) {
+			System.out.println(String.format("Kirja %s ei ole lainassa.", title));
+		} else {
+			copies += 1;
+			borrows -= 1;
+
+			System.out.println(String.format("Kirja %s on palautettu.", title));
+		}
+	}
+}
+
+class NonFictionBook extends Book {
+	NonFictionBook(String title_, String author_, int pages_, int copies_) {
+		title = title_;
+		author = author_;
+
+		pages = pages_;
+		copies = copies_;
+	}
+
+	@Override
+	String categorize() {
+		return "Tietokirja";
+	}
+}
+
+interface Borrowable {
+	void borrow();
+	void returnBook();
+}
+
+class Library {
+	ArrayList<Book> books;
+
+	Library() {
+		books = new ArrayList<Book>();
+	}
+
+	void addBook(Book book_) {
+		books.add(book_);
+	}
+
+	ArrayList<Book> getBooks() {
+		return books;
+	}
+
+	void listBooks() {
+		for (Book book : books) {
+			book.displayinfo();
+
+			System.out.println(String.format("Kirjan kategoria: %s", book.categorize()));
+		}
+	}
+
+	void borrow(String title) {
+		for (Book book : books) {
+			if (book.title == title) {
+				if (book instanceof Borrowable) {
+					((Borrowable)book).borrow();
+
+					System.out.println("Kirja Tuntematon Sotilas on nyt lainattu.");
+
+					return;
+				} else {
+					System.out.println("Kirjaa ei löytynyt tai sitä ei voi lainata.");
+				}
+			}
+		}
+
+		System.out.println("Kirjaa ei löytynyt tai sitä ei voi lainata.");
+	}
+
+	void returnBook(String title) {
+		for (Book book : books) {
+			if (book.title == title) {
+				if (book instanceof Borrowable) {
+					((Borrowable)book).borrow();
+
+					System.out.println("Kirja Tuntematon Sotilas on palautettu.");
+
+					return;
+				} else {
+					System.out.println("Kirjaa ei löytynyt tai sitä ei voi palauttaa.");
+				}
+			}
+		}
+
+		System.out.println("Kirjaa ei löytynyt tai sitä ei voi palauttaa.");
+	}
+}
 
 class Impl {
 	Scanner in;
+	Library library;
 
 	Impl(String[] args) {
 		in = new Scanner(System.in);
+		library = new Library();
 	}
 
 	String nextString() {
@@ -36,21 +175,51 @@ class Impl {
 	}
 
 	void LisaaKirja() {
-		System.out.println("Lisää Kirja");
+		int kind = promtInt("Minkä kirjan haluat lisätä kirjastoon? 1) Fiktiokirja, 2) Tietokirja");
+
+		String title = promtString("Anna kirjan nimi:");
+
+		String author = promtString("Anna kirjailijan nimi:");
+
+		int pages = promtInt("Anna sivumäärä:");
+
+		int copies = promtInt("Anna kirjojen määrä:");
+
+		switch (kind) {
+			case 1:
+				library.addBook(new FictionBook(title, author, pages, copies));
+
+				break;
+
+			case 2:
+				library.addBook(new NonFictionBook(title, author, pages, copies));
+
+				break;
+
+			default:
+				System.out.println("Tuntematon valinta");
+
+				break;
+		}
+
+		System.out.println("Kirja lisätty kirjastoon!");
 	}
 
 	void ListaaKirjat() {
-		System.out.println("Listaa Kirjat");
+		library.listBooks();
 	}
 
 	void LainaaFiktiokirja() {
-		System.out.println("Lainaa fiktiokirja");
+		String title = promtString("Anna lainattavan kirjan nimi:");
+
+		library.borrow(title);
 	}
 
 	void PalautaFiktiokirja() {
-		System.out.println("Palauta fiktiokirja");
-	}
+		String title = promtString("Anna palautettavan kirjan nimi:");
 
+		library.returnBook(title);
+	}
 
 	boolean run() {
 		System.out.println("1) Lisää Kirja");
